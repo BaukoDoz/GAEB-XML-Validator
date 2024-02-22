b0fb01db00c1dba46067e5fed8e506c216638df8
BaukoDoz <47222420+BaukoDoz@users.noreply.github.com>
2024-02-22 14:29:00 +0100

    Update validate_gaeb.py
    Output now with number of errors and 
    f-string formatting

--- validate_gaeb.py
+++ validate_gaeb.py
@@ -34,7 +34,7 @@ def validate_xml(xml_path, xsd_path):
     if len(errors) == 0:
         print("Die XML-Datei ist gültig.")
     else:
-        print("Die XML-Datei ist ungültig. Die Fehler wurden in der Datei 'validation_results.json' gespeichert.")
+        print(f"Die XML-Datei ist ungültig. {len(errors)} Fehler wurden in der Datei 'validation_results.json' gespeichert.")
 
 # Pfad zu den verwendeten Dateien (Pfad zu der 83-GAEB-XML-Datei)
 validate_xml('example.X83', 'GAEB_DA_XML_83_3.3_2021-05.xsd')
